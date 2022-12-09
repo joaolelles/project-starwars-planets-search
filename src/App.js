f44b59914f5b8ea1dd44a2da0e31e4ba40fb806a
João <joaolelles7@gmail.com>
2022-12-09 00:42:51 -0300

    refatorando app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './App.css';
+import StarWarsProvider from './context/StarWarsProvider';
 import Table from './Components/Table';
+import './App.css';
 
 function App() {
   return (
-    <div>
+    <StarWarsProvider>
       <Table />
-    </div>
+    </StarWarsProvider>
   );
 }
 
