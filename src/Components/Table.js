ef612d836ecb49b71d01a33d3a19414f0008fa39
João <joaolelles7@gmail.com>
2022-12-12 13:17:01 -0300

    Requisito 2, Feat: Implementando um filtro de texto para a tabela

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -2,67 +2,91 @@ import React, { useContext, useEffect } from 'react';
 import starWarsContext from '../context/starWarsContext';
 
 function Table() {
-  const { planets, setPlanets } = useContext(starWarsContext);
-
+  const { setPlanets, planets,
+    searchPlanet, setSearchPlanet } = useContext(starWarsContext);
   useEffect(() => {
     const fetchTablePlanets = async () => {
-      const endPoint = 'https://swapi.dev/api/planets';
-      const response = await fetch(endPoint);
-      const data = await response.json();
-      const planetsList = data.results;
-      const deleteResidents = planetsList.map((planet) => {
-        delete planet.residents;
-        return planet;
-      });
-      setPlanets(deleteResidents);
+      try {
+        const endPoint = 'https://swapi.dev/api/planets';
+        const response = await fetch(endPoint);
+        const data = await response.json();
+        const planetsList = data.results;
+        const deleteResidents = planetsList.map((planet) => {
+          delete planet.residents;
+          return planet;
+        });
+        setPlanets(deleteResidents);
+      } catch (err) {
+        console.log(err);
+      }
     };
+    // console.log(fetchTablePlanets());
     fetchTablePlanets();
   }, [setPlanets]);
 
   // Referência do delete:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/delete#:~:text=The%20delete%20operator%20removes%20a,property%20is%20eventually%20released%20automatically.
 
-  const planetInfos = planets;
-  console.log(planetInfos);
+  // const handleFilter = () => {
+  //   const filteredPlanets = planets.filter((planet) => planet.name.toLowerCase()
+  //     .incluedes(searchPlanet.toLowerCase()));
+  //   return filteredPlanets;
+  // };
+
+  console.log(searchPlanet);
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
-      <tbody>
-        {planetInfos.map((planet, index) => (
-          <tr key={ index }>
-            <th>{planet.name}</th>
-            <th>{planet.rotation_period}</th>
-            <th>{planet.orbital_period}</th>
-            <th>{planet.diameter}</th>
-            <th>{planet.climate}</th>
-            <th>{planet.gravity}</th>
-            <th>{planet.terrain}</th>
-            <th>{planet.surface_water}</th>
-            <th>{planet.population}</th>
-            <th>{planet.films}</th>
-            <th>{planet.created}</th>
-            <th>{planet.edited}</th>
-            <th>{planet.url}</th>
+    <div>
+      <input
+        type="text"
+        name="search"
+        data-testid="name-filter"
+        value={ searchPlanet }
+        onChange={ (e) => setSearchPlanet(e.target.value) }
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {planets
+            .filter((planet) => (planet.name.toLowerCase()
+              .includes(searchPlanet.toLowerCase())))
+            .map((planet, index) => (
+              <tr key={ index }>
+                <th>{planet.name}</th>
+                <th>{planet.rotation_period}</th>
+                <th>{planet.orbital_period}</th>
+                <th>{planet.diameter}</th>
+                <th>{planet.climate}</th>
+                <th>{planet.gravity}</th>
+                <th>{planet.terrain}</th>
+                <th>{planet.surface_water}</th>
+                <th>{planet.population}</th>
+                <th>{planet.films}</th>
+                <th>{planet.created}</th>
+                <th>{planet.edited}</th>
+                <th>{planet.url}</th>
+              </tr>
+            ))}
+          ;
+        </tbody>
+      </table>
+    </div>
   );
 }
 
--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import starWarsContext from './starWarsContext';
 
 export default function StarWarsProvider({ children }) {
   const [planets, setPlanets] = useState([]);
+  const [searchPlanet, setSearchPlanet] = useState('');
 
-  const value = {
+  const value = useMemo(() => ({
     planets,
     setPlanets,
-  };
+    searchPlanet,
+    setSearchPlanet,
+  }), [planets, searchPlanet]);
 
   return (
     <starWarsContext.Provider value={ value }>
